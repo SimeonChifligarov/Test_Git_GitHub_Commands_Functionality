c3959fda4e7c6315fe8e39b886a2c211ce381a23
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-04-20 16:24:04 +0300

    from PyCharm Pro; adding some nonsense code

--- just_for_testing_purposes.py
+++ just_for_testing_purposes.py
@@ -20,89 +20,199 @@
 # print(calculate(3, 3, 5))
 # print(calculate(3, 4, 5))
 # print(calculate(3, 2))
-
-def heavy_calculation(*args):
-    arguments = list(*args)
-    result = 0
-    for arg in arguments:
-        result += arg
-    return result
-
-
-def calculate(*args, memo={}):
-    arguments = tuple(args)
-    try:
-        value = memo[arguments]  # return already calculated value
-    except KeyError:
-        value = heavy_calculation(arguments)
-        memo[arguments] = value  # update the memo dictionary
-    return value
-
-
-print(calculate(3, 4, 5))
-print(calculate(3, 2))
-print(calculate(3, 3, 5))
-print(calculate(3, 4, 5))
-print(calculate(3, 2))
-
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-my_dict2 = my_dict.copy()
-my_dict3 = {'a': 1, 'b': 2, 'c': 3}
-my_dict4 = {'b': 1, 'a': 2, 'c': 3}
-my_dict5 = my_dict
-
-print(my_dict == my_dict3)
-print(my_dict == my_dict4)
-print(my_dict == my_dict2)
-print(my_dict == my_dict5)
-
-my_dict2['d'] = 4
-print(my_dict)
-print(my_dict2)
-print(my_dict == my_dict2)
-
-my_list = [1, 2, 3, 4, 5]
-my_list2 = [1, 2, 3, 4, 5]
-my_list3 = my_list.copy()
-
-print(my_list == my_list2 == my_list3)
+#
+# def heavy_calculation(*args):
+#     arguments = list(*args)
+#     result = 0
+#     for arg in arguments:
+#         result += arg
+#     return result
+#
+#
+# def calculate(*args, memo={}):
+#     arguments = tuple(args)
+#     try:
+#         value = memo[arguments]  # return already calculated value
+#     except KeyError:
+#         value = heavy_calculation(arguments)
+#         memo[arguments] = value  # update the memo dictionary
+#     return value
+#
+#
+# print(calculate(3, 4, 5))
+# print(calculate(3, 2))
+# print(calculate(3, 3, 5))
+# print(calculate(3, 4, 5))
+# print(calculate(3, 2))
+#
+# my_dict = {'a': 1, 'b': 2, 'c': 3}
+# my_dict2 = my_dict.copy()
+# my_dict3 = {'a': 1, 'b': 2, 'c': 3}
+# my_dict4 = {'b': 1, 'a': 2, 'c': 3}
+# my_dict5 = my_dict
+#
+# print(my_dict == my_dict3)
+# print(my_dict == my_dict4)
+# print(my_dict == my_dict2)
+# print(my_dict == my_dict5)
+#
+# my_dict2['d'] = 4
+# print(my_dict)
+# print(my_dict2)
+# print(my_dict == my_dict2)
+#
+# my_list = [1, 2, 3, 4, 5]
+# my_list2 = [1, 2, 3, 4, 5]
+# my_list3 = my_list.copy()
+#
+# print(my_list == my_list2 == my_list3)
+# # print(my_list == my_list3)
+# # print(my_list3 == my_list2)
+# my_list2.append(100)
+# print(my_list == my_list2)
 # print(my_list == my_list3)
-# print(my_list3 == my_list2)
-my_list2.append(100)
-print(my_list == my_list2)
-print(my_list == my_list3)
+#
+# my_list = [1, 2, 3, 4, 5]
+# my_list2 = [1, 2, 3, 4, 5]
+# my_list3 = my_list.copy()
+# my_list4 = my_list
+#
+# print(my_list == my_list2)
+# print(id(my_list) == id(my_list2))
+# print(id(my_list) == id(my_list3))
+# print(id(my_list) == id(my_list4))
+#
+# a = []
+# a.pop() # IndexError: pop from empty list
+# a = {'a': 1, 'b': 2, 'c': 3}
+# b = a.popitem()
+# print(b)
+# print(a)
+#
+# a = (1, 250, 3, 43, 2, 11)
+# print(sorted(a))
+# print(a)
+# a.sort()
+# print(a)
+#
+# a = ['ab', 'bbbbbbbb', 'Az', 'az', '11', 'Ca']
+# print(sorted(a))
+# print(a)
+# a.sort()
+# print(a)
 
-my_list = [1, 2, 3, 4, 5]
-my_list2 = [1, 2, 3, 4, 5]
-my_list3 = my_list.copy()
-my_list4 = my_list
-
-print(my_list == my_list2)
-print(id(my_list) == id(my_list2))
-print(id(my_list) == id(my_list3))
-print(id(my_list) == id(my_list4))
-
-a = []
-a.pop() # IndexError: pop from empty list
-a = {'a': 1, 'b': 2, 'c': 3}
-b = a.popitem()
-print(b)
-print(a)
-
-a = (1, 250, 3, 43, 2, 11)
-print(sorted(a))
-print(a)
-a.sort()
-print(a)
-
-a = ['ab', 'bbbbbbbb', 'Az', 'az', '11', 'Ca']
-print(sorted(a))
-print(a)
-a.sort()
-print(a)
+# ### received
+# gimme *conflict* 
 
 def sort_me_baby(el1):
     return el1[0]
 
-# ### received
-# gimme *conflict* 
+
+a = {'a': 22, 'd': 14, 'b': 33}
+b = sorted(a.items(), key=lambda x: x[0])
+print(b)
+c = dict(b)
+print(c)
+d = sorted(a.items(), key=sort_me_baby)
+print(dict(d))
+print(*a.items(), sep=', ')
+
+b = ('a', 22)
+print(*b)
+
+a = 'with     5spaces baby'
+print(a.split(' ', maxsplit=1))
+print(a.split(' ', maxsplit=2))
+print(a.split(' ', maxsplit=3))
+print(a.split(' ', maxsplit=4))
+print(a.split(' ', maxsplit=5))
+print(a.split(' ', maxsplit=6))
+print(a.split(' ', maxsplit=7))
+print(a.split(' ', maxsplit=8))
+print(a.split(' '))
+print(' '.join(a.split(' ')))
+
+a = '   '
+print(a.split(' '))
+
+a = 'abcdef'
+print(''.join(list(reversed(a))))
+
+print('b1'.isalpha())
+
+a = ['baba', 'asdasdasd', 'kick', 'da', 'manastirski livadi']
+b = (len(el) for el in a)
+print(b)
+
+a = [1, 2, 3, 4, 5, 6]
+# changed = [True if num % 2 == 0 else 'baba' for num in a]
+changed = [11 if num % 2 == 0 else 'baba' for num in a]
+
+print(changed)
+
+a = [1, 2, 3, 4]
+b = [1, [2, 3, 4], [5, 6]]
+a.extend(b)
+print(a)
+print(b)
+a.insert(0, 100)
+print(a)
+
+
+a = {'baba': 3, 'dqdo': 11, 'kote': 33, 'kuchence': 0}
+b = sorted(a.items(), key=lambda el: el[1])
+c = [el[0] for el in b]
+print(b)
+print(c)
+
+d = list(a.keys())
+e = list(a.values())
+print(d)
+print(sorted(e))
+print(e)
+
+
+a = [1, 2, 3, 4, 5, 6]
+# b = list(reversed(a))
+b = reversed(a)
+c = a[::-1]
+print(a)
+# print(b)
+# print(list(b))
+print(c)
+
+a.reverse()
+print(a)
+# print(b)
+print(list(b))
+print(c)
+
+a = [1, 2, 3, 4, 5, 6]
+b = map(lambda el: str(el) + ' boza', a)
+print(b)
+print(list(b))
+
+a = ['a', 'b', 'baba', 'dqdo']
+a[0], a[1], a[3] = a[2], a[3], a[0]
+print(a)
+
+import re
+
+a = 'some fucking good text11233'
+b = list(a)
+# print(re.split(r'', a)[1:-1])
+# print(re.findall(r'.', a))
+
+c = ''.join(b)
+print(c)
+print(''.join(b))
+
+from collections import deque
+
+a = [1, 2, 3, 4, 5, 6]
+b = deque([str(el) for el in a])
+print(b)
+print(type(b))
+# print('-------------')
+# print('\n'.join(b))
+print(b[2] + b[3])
