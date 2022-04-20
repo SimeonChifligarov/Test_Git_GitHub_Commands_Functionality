2fb4e957fabd44334ce5b58f87e8acbb63d589b9
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-04-20 16:32:54 +0300

    commiting some non-valueable code from GitBash again

--- just_for_testing_purposes.py
+++ just_for_testing_purposes.py
@@ -104,117 +104,158 @@
 # ### received
 # gimme *conflict* 
 
-def sort_me_baby(el1):
-    return el1[0]
-
-
-a = {'a': 22, 'd': 14, 'b': 33}
-b = sorted(a.items(), key=lambda x: x[0])
-print(b)
-c = dict(b)
-print(c)
-d = sorted(a.items(), key=sort_me_baby)
-print(dict(d))
-print(*a.items(), sep=', ')
-
-b = ('a', 22)
-print(*b)
-
-a = 'with     5spaces baby'
-print(a.split(' ', maxsplit=1))
-print(a.split(' ', maxsplit=2))
-print(a.split(' ', maxsplit=3))
-print(a.split(' ', maxsplit=4))
-print(a.split(' ', maxsplit=5))
-print(a.split(' ', maxsplit=6))
-print(a.split(' ', maxsplit=7))
-print(a.split(' ', maxsplit=8))
-print(a.split(' '))
-print(' '.join(a.split(' ')))
-
-a = '   '
-print(a.split(' '))
-
-a = 'abcdef'
-print(''.join(list(reversed(a))))
-
-print('b1'.isalpha())
-
-a = ['baba', 'asdasdasd', 'kick', 'da', 'manastirski livadi']
-b = (len(el) for el in a)
-print(b)
-
-a = [1, 2, 3, 4, 5, 6]
-# changed = [True if num % 2 == 0 else 'baba' for num in a]
-changed = [11 if num % 2 == 0 else 'baba' for num in a]
-
-print(changed)
-
-a = [1, 2, 3, 4]
-b = [1, [2, 3, 4], [5, 6]]
-a.extend(b)
-print(a)
-print(b)
-a.insert(0, 100)
-print(a)
-
-
-a = {'baba': 3, 'dqdo': 11, 'kote': 33, 'kuchence': 0}
-b = sorted(a.items(), key=lambda el: el[1])
-c = [el[0] for el in b]
-print(b)
-print(c)
-
-d = list(a.keys())
-e = list(a.values())
-print(d)
-print(sorted(e))
-print(e)
-
-
-a = [1, 2, 3, 4, 5, 6]
-# b = list(reversed(a))
-b = reversed(a)
-c = a[::-1]
-print(a)
+# def sort_me_baby(el1):
+#     return el1[0]
+#
+#
+# a = {'a': 22, 'd': 14, 'b': 33}
+# b = sorted(a.items(), key=lambda x: x[0])
+# print(b)
+# c = dict(b)
+# print(c)
+# d = sorted(a.items(), key=sort_me_baby)
+# print(dict(d))
+# print(*a.items(), sep=', ')
+#
+# b = ('a', 22)
+# print(*b)
+#
+# a = 'with     5spaces baby'
+# print(a.split(' ', maxsplit=1))
+# print(a.split(' ', maxsplit=2))
+# print(a.split(' ', maxsplit=3))
+# print(a.split(' ', maxsplit=4))
+# print(a.split(' ', maxsplit=5))
+# print(a.split(' ', maxsplit=6))
+# print(a.split(' ', maxsplit=7))
+# print(a.split(' ', maxsplit=8))
+# print(a.split(' '))
+# print(' '.join(a.split(' ')))
+#
+# a = '   '
+# print(a.split(' '))
+#
+# a = 'abcdef'
+# print(''.join(list(reversed(a))))
+#
+# print('b1'.isalpha())
+#
+# a = ['baba', 'asdasdasd', 'kick', 'da', 'manastirski livadi']
+# b = (len(el) for el in a)
+# print(b)
+#
+# a = [1, 2, 3, 4, 5, 6]
+# # changed = [True if num % 2 == 0 else 'baba' for num in a]
+# changed = [11 if num % 2 == 0 else 'baba' for num in a]
+#
+# print(changed)
+#
+# a = [1, 2, 3, 4]
+# b = [1, [2, 3, 4], [5, 6]]
+# a.extend(b)
+# print(a)
+# print(b)
+# a.insert(0, 100)
+# print(a)
+#
+#
+# a = {'baba': 3, 'dqdo': 11, 'kote': 33, 'kuchence': 0}
+# b = sorted(a.items(), key=lambda el: el[1])
+# c = [el[0] for el in b]
+# print(b)
+# print(c)
+#
+# d = list(a.keys())
+# e = list(a.values())
+# print(d)
+# print(sorted(e))
+# print(e)
+#
+#
+# a = [1, 2, 3, 4, 5, 6]
+# # b = list(reversed(a))
+# b = reversed(a)
+# c = a[::-1]
+# print(a)
+# # print(b)
+# # print(list(b))
+# print(c)
+#
+# a.reverse()
+# print(a)
+# # print(b)
+# print(list(b))
+# print(c)
+#
+# a = [1, 2, 3, 4, 5, 6]
+# b = map(lambda el: str(el) + ' boza', a)
 # print(b)
 # print(list(b))
-print(c)
-
-a.reverse()
-print(a)
+#
+# a = ['a', 'b', 'baba', 'dqdo']
+# a[0], a[1], a[3] = a[2], a[3], a[0]
+# print(a)
+#
+# import re
+#
+# a = 'some fucking good text11233'
+# b = list(a)
+# # print(re.split(r'', a)[1:-1])
+# # print(re.findall(r'.', a))
+#
+# c = ''.join(b)
+# print(c)
+# print(''.join(b))
+#
+# from collections import deque
+#
+# a = [1, 2, 3, 4, 5, 6]
+# b = deque([str(el) for el in a])
 # print(b)
-print(list(b))
-print(c)
+# print(type(b))
+# # print('-------------')
+# # print('\n'.join(b))
+# print(b[2] + b[3])
+#
+# # Try to find BUG
 
-a = [1, 2, 3, 4, 5, 6]
-b = map(lambda el: str(el) + ' boza', a)
-print(b)
-print(list(b))
 
-a = ['a', 'b', 'baba', 'dqdo']
-a[0], a[1], a[3] = a[2], a[3], a[0]
+a = (['a', 'b', 'c'], [1, 2, 3], [4, 5, 6],)
+b = [1, ]
+c = []
+
+# a[1].append(4)
+a[1].clear()
 print(a)
-
-import re
-
-a = 'some fucking good text11233'
-b = list(a)
-# print(re.split(r'', a)[1:-1])
-# print(re.findall(r'.', a))
-
-c = ''.join(b)
-print(c)
-print(''.join(b))
-
-from collections import deque
-
-a = [1, 2, 3, 4, 5, 6]
-b = deque([str(el) for el in a])
 print(b)
-print(type(b))
-# print('-------------')
-# print('\n'.join(b))
-print(b[2] + b[3])
+print(c)
+print(len(a))
 
-# Try to find BUG
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6, 7, 8, 9}
+print(a.union(b) == b.union(a))
+from pprint import pprint
+
+a = []
+for i in range(5):
+    a.append([j + 1 for j in range(5)])
+
+# pprint(a)
+
+# b = [1, 2, 3, 4]
+# [print(row) for row in [el for el in a]]
+
+a = 'basi qkoto maina11'
+b = [ch for ch in a]
+print(b)
+c = list(a)
+print(c)
+print('----------')
+print(b == a)
+print(c == list(a))
+d = list(a)
+print(c == d)
+d.pop()
+print(d)
+print(c)
+print(c == d)
