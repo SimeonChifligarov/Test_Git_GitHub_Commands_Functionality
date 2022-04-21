877b98a4d11cf059a851464fca74fb39dc3d2562
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-04-21 17:52:51 +0300

    some python code of the day XYZ

--- just_for_testing_purposes.py
+++ just_for_testing_purposes.py
@@ -218,44 +218,147 @@
 # print(b[2] + b[3])
 #
 # # Try to find BUG
+#
+#
+# a = (['a', 'b', 'c'], [1, 2, 3], [4, 5, 6],)
+# b = [1, ]
+# c = []
+#
+# # a[1].append(4)
+# a[1].clear()
+# print(a)
+# print(b)
+# print(c)
+# print(len(a))
+#
+# a = {1, 2, 3, 4}
+# b = {3, 4, 5, 6, 7, 8, 9}
+# print(a.union(b) == b.union(a))
+# from pprint import pprint
+#
+# a = []
+# for i in range(5):
+#     a.append([j + 1 for j in range(5)])
+#
+# # pprint(a)
+#
+# # b = [1, 2, 3, 4]
+# # [print(row) for row in [el for el in a]]
+#
+# a = 'basi qkoto maina11'
+# b = [ch for ch in a]
+# print(b)
+# c = list(a)
+# print(c)
+# print('----------')
+# print(b == a)
+# print(c == list(a))
+# d = list(a)
+# print(c == d)
+# d.pop()
+# print(d)
+# print(c)
+# print(c == d)
 
+a = 'bate agoshooo'
+b = set(a)
+print(b)
 
-a = (['a', 'b', 'c'], [1, 2, 3], [4, 5, 6],)
-b = [1, ]
-c = []
-
-# a[1].append(4)
-a[1].clear()
+a = range(5)
+a = (i for i in range(5))
 print(a)
-print(b)
-print(c)
-print(len(a))
 
-a = {1, 2, 3, 4}
-b = {3, 4, 5, 6, 7, 8, 9}
-print(a.union(b) == b.union(a))
-from pprint import pprint
+for i in a:
+    print(i)
+for i in a:
+    print(i)
+for i in a:
+    print(i)
 
-a = []
-for i in range(5):
-    a.append([j + 1 for j in range(5)])
+print(type(a))
+print(a)
 
-# pprint(a)
+a = {1}
+print(type(a))
+a.add(5)
+print(a)
 
-# b = [1, 2, 3, 4]
-# [print(row) for row in [el for el in a]]
+def is_vowel(char):
+    return char in ['a', 'o', 'e']
 
-a = 'basi qkoto maina11'
-b = [ch for ch in a]
-print(b)
-c = list(a)
-print(c)
-print('----------')
-print(b == a)
-print(c == list(a))
-d = list(a)
-print(c == d)
-d.pop()
-print(d)
-print(c)
-print(c == d)
+
+print(is_vowel('o'))
+print(is_vowel('x'))
+
+a = [1, 2, 3, 4, 5]
+b = []
+print(len(a) > 0)
+print(len(b) > 0)
+
+print(bool(a))
+print(bool(b))
+
+a = [0, 0, 1]
+print(any(a))
+
+print(True in a)
+
+def some_func(*args, **kwargs):
+    print(type(args))
+    print(args)
+    print(*args)
+    print('-----------')
+    print(type(kwargs))
+    print(kwargs)
+    # print(**kwargs)
+    real_dict = kwargs
+    print(real_dict)
+
+some_func(1, 2, 3, 4, 5, boss='mass', mad='duddddeeee')
+
+def my111_func(**kwargs):
+    print(kwargs)
+
+
+a = {'azd': 1, 'bab': 4, 'karl': 9, 'linch': 16}
+# print(**a)
+my111_func(**a)
+
+def my22_func(name, age):
+    return f'My name is {name}, and my age is {age}'
+
+
+a = {'name': 'Gosho', 'age': 15}
+b = {'name': 'Gosho', 'age': 15, 'mass': 'azzzz'}
+
+print(my22_func(**a))
+# print(my22_func(**b))
+
+def a(n):
+    if n == 0:
+        return
+    print(n)
+    a(n - 1)
+
+
+a(5)
+
+import sys
+
+print(sys.version)
+
+my_arr = ['kuche', 'baba', 'kotka', 'kotka', 'baba', 'dqdo', 'kuche', 'kotka']
+my_dict = {word: my_arr.count(word) for word in my_arr}
+print(my_dict)
+
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+
+# Identify odd and even entries
+res = {k: ('Even' if v % 2 == 0 else 'Odd') for (k, v) in dict1.items()}
+
+print(res)
+
+my_arr0 = ['kuche', 'kotka']
+my_arr = ['kuche', 'baba', 'kotka', 'kotka', 'baba', 'dqdo', 'kuche', 'kotka']
+my_dict0 = {word: my_arr.count(word) for word in my_arr0}
+print(my_dict0)
