f3829e0f93cb9c7b4ac0553341c717d265a5a178
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-05-24 14:40:43 +0300

    making file clear (commenting, ctr+/)

--- python_basic_functionality_testing.py
+++ python_basic_functionality_testing.py
@@ -183,164 +183,164 @@
 
 # a = [1, 2, 3, 'abc', 'def', 4, 2, 1, 'a', 'abb']
 # print(sorted(a))  # TypeError: '<' not supported between instances of 'str' and 'int'
+# 
+# 
+# a = {'1a': 1, '2ab': 2, '3abc': 3, '1a': 0}  # Dictionary contains duplicate keys '1a'
+# print(a)
+# print([el for el in enumerate(a)])
+# a['1a'] = 11
+# print(a)
+# print([el for el in enumerate(a)])
 
+# b = dict(many='true', yes='da', **a)
+# print(b)
+# c = dict(no='ne', **a, be='non-be')
+# print(c)
+# print(c.keys())  # dict_keys
+# print(c.values())  # dict_values
+# print(c.items())  # dict_items
+# print(list(c.keys()))
 
-a = {'1a': 1, '2ab': 2, '3abc': 3, '1a': 0}  # Dictionary contains duplicate keys '1a'
-print(a)
-print([el for el in enumerate(a)])
-a['1a'] = 11
-print(a)
-print([el for el in enumerate(a)])
+# x = {1: 'one', 2: 'two', 3: 'three'}
+# y = {1: 'one', 2: 'two', 3: 'three'}
+# z = {2: 'two', 3: 'three', 1: 'one'}
+# print(x == y)  # True
+# print(y == z)  # True
+# print(x == y == z)  # True
+# print(id(x) == id(y))  # False
+# z1 = z.copy()
+# print(z1 == z)  # True
+# z1[1337] = 'more_value'
+# print(z1 == z)  # False
+# print(z1)
 
-b = dict(many='true', yes='da', **a)
-print(b)
-c = dict(no='ne', **a, be='non-be')
-print(c)
-print(c.keys())  # dict_keys
-print(c.values())  # dict_values
-print(c.items())  # dict_items
-print(list(c.keys()))
+# print('---------')
+# a1 = {'b', 'd', 'a', 13, 37}
 
-x = {1: 'one', 2: 'two', 3: 'three'}
-y = {1: 'one', 2: 'two', 3: 'three'}
-z = {2: 'two', 3: 'three', 1: 'one'}
-print(x == y)  # True
-print(y == z)  # True
-print(x == y == z)  # True
-print(id(x) == id(y))  # False
-z1 = z.copy()
-print(z1 == z)  # True
-z1[1337] = 'more_value'
-print(z1 == z)  # False
-print(z1)
+# print(a1)
+# print([el for el in enumerate(a1)])
+# a1.add('lamqta')
+# print(a1)
+# print([el for el in enumerate(a1)])
 
-print('---------')
-a1 = {'b', 'd', 'a', 13, 37}
+# y = {1: 'one', 2: 'two', 3: 'three'}
+# z = {2: 'two', 3: 'three', 1: 'one'}
+# print(y == z)  # True
+# print(y.popitem())
+# print(z.popitem())
+# print(y == z)  # False
 
-print(a1)
-print([el for el in enumerate(a1)])
-a1.add('lamqta')
-print(a1)
-print([el for el in enumerate(a1)])
+# employees = {1: {'name': 'Pete', 'age': 22},
+#              2: {'name': 'Alex', 'age': 21}}
 
-y = {1: 'one', 2: 'two', 3: 'three'}
-z = {2: 'two', 3: 'three', 1: 'one'}
-print(y == z)  # True
-print(y.popitem())
-print(z.popitem())
-print(y == z)  # False
+# employees[3] = {}  # {3: {}}
+# employees[3]['name'] = 'Anna'  # {3: {'name': 'Anna'}}
+# employees[3]['age'] = 25  # {3: {'name': 'Anna', 'age': 25}}
 
-employees = {1: {'name': 'Pete', 'age': 22},
-             2: {'name': 'Alex', 'age': 21}}
+# print(employees)
+# employees[4] = dict(name='Monster', age=88)
+# print(employees)
 
-employees[3] = {}  # {3: {}}
-employees[3]['name'] = 'Anna'  # {3: {'name': 'Anna'}}
-employees[3]['age'] = 25  # {3: {'name': 'Anna', 'age': 25}}
+# a = "a   d e  1 2"
+# b = a.split()
+# print(b)
+# c = a.split(maxsplit=2)
+# print(c)  # ['a', 'd', 'e  1 2']
+# print(a[::-1])
 
-print(employees)
-employees[4] = dict(name='Monster', age=88)
-print(employees)
+# some_string = 'My name is Zimeon'
+# print(some_string[-6:])
+# # print(len(some_string))
 
-a = "a   d e  1 2"
-b = a.split()
-print(b)
-c = a.split(maxsplit=2)
-print(c)  # ['a', 'd', 'e  1 2']
-print(a[::-1])
+# print('12123'.isdigit())  # True
+# print('121212q33'.isdigit())  # False
+# print('1'.isupper())  # False
+# print('1'.islower())  # False
+# print('ABC2'.isupper())  # True
+# print('ABCc'.isupper())  # False
 
-some_string = 'My name is Zimeon'
-print(some_string[-6:])
-# print(len(some_string))
+# text = input()
+# open_parentheses = []
 
-print('12123'.isdigit())  # True
-print('121212q33'.isdigit())  # False
-print('1'.isupper())  # False
-print('1'.islower())  # False
-print('ABC2'.isupper())  # True
-print('ABCc'.isupper())  # False
+# for i in range(len(text)):
+#     if text[i] == "(":
+#         open_parentheses.append(i)
+#     elif text[i] == ")":
+#         start_index = open_parentheses.pop()
+#         print(text[start_index:i + 1])
 
-text = input()
-open_parentheses = []
+# from collections import deque
+# queue = deque(["Pesho", "Gosho", "Tosho"])  # append, popleft; double-ended que => appendleft, pop
 
-for i in range(len(text)):
-    if text[i] == "(":
-        open_parentheses.append(i)
-    elif text[i] == ")":
-        start_index = open_parentheses.pop()
-        print(text[start_index:i + 1])
+# a = [1, 2]
+# b = (3, 4)
+# c, d = a
+# e, f = b
+# # k, l, m = b  # ValueError: not enough values to unpack (expected 3, got 2)
 
-from collections import deque
-queue = deque(["Pesho", "Gosho", "Tosho"])  # append, popleft; double-ended que => appendleft, pop
+# print(c, d)
+# print(e)
+# print(f)
 
-a = [1, 2]
-b = (3, 4)
-c, d = a
-e, f = b
-# k, l, m = b  # ValueError: not enough values to unpack (expected 3, got 2)
+# a = []
+# b = 4
+# c = (a, b)
+# print(c)
 
-print(c, d)
-print(e)
-print(f)
+# a.append(4)
+# a.append(41)
+# a.append(24)
+# a.append(444)
+# print(c)
+# a.append(a)
+# print(c)
+# print(a[-1])
+# print(a)
+# print(a[-1] == a)  # True
 
-a = []
-b = 4
-c = (a, b)
-print(c)
+# a = 1,
+# print(a)
+# print(type(a))  # <class 'tuple'>
+# print(a[0])
+# # a[0] = 2  # TypeError: 'tuple' object does not support item assignment
 
-a.append(4)
-a.append(41)
-a.append(24)
-a.append(444)
-print(c)
-a.append(a)
-print(c)
-print(a[-1])
-print(a)
-print(a[-1] == a)  # True
+# a = {1}
+# print(type(a))
+# a.pop()
+# print(a)
+# # a.pop()  # KeyError: 'pop from an empty set'
 
-a = 1,
-print(a)
-print(type(a))  # <class 'tuple'>
-print(a[0])
-# a[0] = 2  # TypeError: 'tuple' object does not support item assignment
+# matrix = [[0 for _ in range(2)] for _ in range(3)]
+# print(matrix)
 
-a = {1}
-print(type(a))
-a.pop()
-print(a)
-# a.pop()  # KeyError: 'pop from an empty set'
+# matrix1 = [[j for j in range(1, 4)] for i in range(3)]
+# matrix2 = [[j + i * 3 for j in range(1, 4)] for i in range(3)]
+# print(matrix1)
+# print(matrix2)
 
-matrix = [[0 for _ in range(2)] for _ in range(3)]
-print(matrix)
+# matrix = [[1, 2, 3], [4, 5, 6]]
+# flattened1 = [num for sublist in matrix for num in sublist]
+# flattened2 = [num * 3 for sublist in matrix for num in sublist]
 
-matrix1 = [[j for j in range(1, 4)] for i in range(3)]
-matrix2 = [[j + i * 3 for j in range(1, 4)] for i in range(3)]
-print(matrix1)
-print(matrix2)
+# print(flattened1)
+# print(flattened2)
 
-matrix = [[1, 2, 3], [4, 5, 6]]
-flattened1 = [num for sublist in matrix for num in sublist]
-flattened2 = [num * 3 for sublist in matrix for num in sublist]
+# from pprint import pprint
 
-print(flattened1)
-print(flattened2)
+# rows_count = 10
+# # matrix = [map(int, ['1', '13', '133', '1337']) for _ in range(rows_count)]
+# matrix = [list(map(int, ['1', '13', '133', '1337'])) for _ in range(rows_count)]
 
-from pprint import pprint
+# pprint(matrix)
 
-rows_count = 10
-# matrix = [map(int, ['1', '13', '133', '1337']) for _ in range(rows_count)]
-matrix = [list(map(int, ['1', '13', '133', '1337'])) for _ in range(rows_count)]
+# flattened = [num for row in matrix for num in row]
+# print(flattened)
 
-pprint(matrix)
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# for i in range(len(matrix)):
+#     for j in range(len(matrix[i])):
+#         print(matrix[i][j], end=" ")
+#     print()
 
-flattened = [num for row in matrix for num in row]
-print(flattened)
-
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        print(matrix[i][j], end=" ")
-    print()
-
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print([j for j in matrix] == matrix)  # True
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# print([j for j in matrix] == matrix)  # True
