c0b213da84e6ab5c6375eb549c7fa2bfb7f9e9c0
SimeonChifligarov <simeon.chifligarov@gmail.com>
2022-05-25 13:33:41 +0300

    adding some daily code 2, git push

--- python_basic_functionality_testing.py
+++ python_basic_functionality_testing.py
@@ -681,3 +681,86 @@
 # 
 # print(list(genrange(1, 10)))  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # 
+
+from time import time
+
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start = time()
+        result = func(*args, **kwargs)
+        end = time()
+        print(end - start)
+        return result
+
+    return wrapper
+
+
+@measure_time
+def append_1000():
+    ll = []
+    for i in range(1_000_000):
+        ll.append(i ** 3)
+
+    return ll
+
+
+append_1000()
+
+class Fibonacci:
+    def __init__(self):
+        self.cache = {}
+
+    def __call__(self, n):
+        if n not in self.cache:
+            if n == 0:
+                self.cache[0] = 0
+            elif n == 1:
+                self.cache[1] = 1
+            else:
+                self.cache[n] = self(n - 1) + self(n - 2)
+        return self.cache[n]
+
+
+fib = Fibonacci()
+
+for i in range(5):
+    print(fib(i))
+
+print(fib.cache)
+
+class func_logger:
+    _logfile = 'out.log'
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        log_string = self.func.__name__ + " was called"
+        with open(self._logfile, 'a') as opened_file:
+            opened_file.write(log_string + '\n')
+        return self.func(*args)
+
+
+@func_logger
+def say_hi(name):
+    print(f"Hi, {name}")
+
+
+@func_logger
+def say_bye(name):
+    print(f"Bye, {name}")
+
+
+say_hi("Peter")
+say_bye("Peter")
+
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+c = [el for el in a]
+print(a, b, c)
+
+print(a == b == c)  # True
+print(a is b)  # False
+print(b is c)  # False
+print(b == c)  # True
